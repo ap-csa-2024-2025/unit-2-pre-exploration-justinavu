e21f88cd671acf1f34f184716380cd9a35d2da1c
justinavu <justinvu2008@gmail.com>
2024-09-24 09:46:34 -0700

    sync

--- WarmUp.java
+++ WarmUp.java
@@ -2,10 +2,17 @@ public class WarmUp
 {
   public static void main(String[] args)
   {
-    // test your methods here
+    boolean real = isDivisible(25, 5);
+    boolean even = isEven(8);
+    System.out.println("isDivisible = " + real + ", isEven = " + even);
   }
-
-  // write your methods below
-
+  public static boolean isEven(int n)
+  {
+    return (n % 2 == 0);
+  }
+  public static boolean isDivisible(int dividend, int divisor)
+  {
+    return (dividend % divisor == 0);
+  }
   
 }
